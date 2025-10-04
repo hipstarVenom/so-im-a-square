654c856658abe30010a4f03753cb3f13ac980483
hipstarVenom <yuvansakthi046@gmail.com>
2025-10-04 18:55:15 +0530

    feat : animation for up and down

--- so-i-am-a-square-so-what-/Code/Player/player.gd
+++ so-i-am-a-square-so-what-/Code/Player/player.gd
@@ -2,19 +2,41 @@ extends CharacterBody2D
 
 @onready var anim = $AnimatedSprite2D
 var speed := 200
+var last_facing := "down"  
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2.ZERO 
+	velocity = Vector2.ZERO
+	
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += speed
+		velocity.x = speed
 		anim.play("move_right")
 		anim.flip_h = false
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x -= speed
-		anim.play("move_right")
-		anim.flip_h=true
-	else :
-		anim.play("idle_left_right")
+		last_facing = "right"
 		
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		anim.play("move_right") 
+		anim.flip_h = true
+		last_facing = "left"
+		
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = speed
+		anim.play("move_down")
+		last_facing = "down"
+		
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = -speed
+		anim.play("move_up")
+		last_facing = "up"
+		
+	else:
+		match last_facing:
+			"right", "left":
+				anim.play("idle_left_right")
+				anim.flip_h = (last_facing == "left")
+			"down":
+				anim.play("idle_down")
+			"up":
+				anim.play("idle_up")
+
 	move_and_slide()
-	
